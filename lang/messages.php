fdd91fda927342f28fd9ba0153e11645b6e213c3
pihedy <pihedy@gmail.com>
2020-12-09 07:48:01 +0100

    The messages file has been expanded.

--- lang/messages.php
+++ lang/messages.php
@@ -1,8 +1,17 @@
 <?php declare(strict_types=1);
 
+/** 
+ * Simple array with translatable texts.
+ * 
+ * @author Pihe Edmond <pihedy@gmail.com>
+ */
+
 return [
     'error' => [
         'bootstrap_not_found'   => __('Bootstrap file not found!', 'goat'),
-        'install_composer'      => __('Before install composer!', 'goat')
+        'install_composer'      => __('Before install composer!', 'goat'),
+        'providers_not_found'   => __('The providers array cannot be empty!', 'goat'),
+        'invalid_container'     => __('Container incorrectly specified.', 'goat'),
+        'singleton_problem'     => __('Cannot unserialize a singleton.', 'goat')
     ]
 ];
