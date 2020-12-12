073610fa0c9969f894c2ab0687446c09bd8eae78
pihedy <pihedy@gmail.com>
2020-12-12 19:15:22 +0100

    Message handling class and provider created.

---
+++ src/app/Foundation/Messages.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types=1);
+
+namespace Goat\Foundation;
+
+/** 
+ * The department that handles other messages.
+ * 
+ * @author Pihe Edmond <pihedy@gmail.com>
+ */
+final class Messages
+{
+    /**
+     * Loaded messages to search.
+     * 
+     * @var array
+     */
+    protected $messages;
+
+    /**
+     * @param array $messages Received messages in which to search.
+     */
+    public function __construct(array $messages)
+    {
+        $this->messages = $messages;
+    }
+
+    /**
+     * Returns the message based on the key.
+     * 
+     * @param string $key 
+     * 
+     * @return null|string 
+     */
+    public function get(string $key): ?string
+    {
+        if (array_key_exists($key, $this->messages)) {
+            return $this->messages[$key];
+        }
+
+        $segments   = explode('.', $key);
+        $root       = $this->messages;
+
+        foreach ($segments as $segment) {
+            if (array_key_exists($segment, $root)) {
+                $root = $root[$segment];
+
+                continue;
+            } else {
+                return null;
+            }
+        }
+
+        return $root;
+    }
+}
---
+++ src/app/Foundation/Providers/MessageProvider.php
@@ -0,0 +1,97 @@
+<?php declare(strict_types=1);
+
+namespace Goat\Foundation\Providers;
+
+use \Goat\Interfaces\Provider;
+use \Goat\Traits\ProviderTrait;
+use \Goat\Foundation\Messages;
+
+/** 
+ * Register messages to Goat.
+ * 
+ * @author Pihe Edmond <pihedy@gmail.com>
+ */
+class MessageProvider implements Provider
+{
+    use ProviderTrait;
+
+    /**
+     * Registration keys.
+     * 
+     * @var array
+     */
+    protected $registers = [
+        'Messags',
+    ];
+
+    /**
+     * The folder that contains the messages.
+     * 
+     * @var string
+     */
+    protected $directory;
+
+    /**
+     * return void
+     */
+    public function __construct()
+    {
+        /** 
+         * @hook Here you can override the base folder where the message files are.
+         */
+        $this->directory = apply_filters(
+            'turbo_goat_message_core_directory',
+            GOAT_ROOT . DIRECTORY_SEPARATOR . 'lang'
+        );
+    }
+
+    /**
+     * It registers the files and passes them to the message manager.
+     * 
+     * @return void 
+     */
+    public function registerMessags(): void
+    {
+        /** 
+         * @hook The filter can be used to expand the language container.
+         */
+        $directories = array_merge(
+            [
+                $this->directory
+            ],
+            apply_filters('turbo_goat_message_directories', [])
+        );
+
+        $messages = [];
+
+        foreach ($directories as $directory) {
+            if (!is_dir($directory)) {
+                continue;
+            }
+
+            $DirectoryIterator = new \DirectoryIterator($directory);
+            
+            foreach ($DirectoryIterator as $Content) {
+                if ($Content->isDot() || $Content->isDir()) {
+                    continue;
+                }
+
+                if ($Content->getExtension() != 'php') {
+                    continue;
+                }
+
+                $fileContent = include $Content->getPathname();
+
+                if (!is_array($fileContent) || empty($fileContent)) {
+                    continue;
+                }
+
+                $messages = array_merge($messages, [
+                    $Content->getBasename('.php') => $fileContent
+                ]);
+            }
+        }
+
+        goat()->Container->set('messages', new Messages($messages));
+    }
+}
