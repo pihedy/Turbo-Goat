2084af34d738b735be7012ebdd7d52a1fdfb8f02
pihedy <pihedy@gmail.com>
2021-01-14 22:58:48 +0100

    Remove two error message.

--- lang/errors.php
+++ lang/errors.php
@@ -7,8 +7,6 @@
  */
 
 return [
-    'bootstrap_not_found'   => __('Bootstrap file not found!', 'goat'),
-    'install_composer'      => __('Before install composer!', 'goat'),
     'providers_not_found'   => __('The providers array cannot be empty!', 'goat'),
     'invalid_container'     => __('Container incorrectly specified.', 'goat'),
     'singleton_problem'     => __('Cannot unserialize a singleton.', 'goat')
