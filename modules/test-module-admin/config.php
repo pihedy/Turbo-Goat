1065e6e12479ca50179e7b49a7ad807ff5a03fd8
pihedy <pihedy@gmail.com>
2021-03-29 21:58:04 +0200

    Add author email to config.

--- modules/test-module-admin/config.php
+++ modules/test-module-admin/config.php
@@ -5,7 +5,8 @@ return [
     'display_name'  => 'Test Module Admin',
     'version'       => '0.1.0',
     'description'   => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    'author'        => 'Pihe Edmond <pihedy@gmail.com>',
+    'author'        => 'Pihe Edmond',
+    'email'         => 'pihedy@gmail.com',
     'side'          => 'admin',
     'has_tab'       => true,
     'start'         => 'test.php'
--- modules/test-module-site/config.php
+++ modules/test-module-site/config.php
@@ -5,7 +5,8 @@ return [
     'display_name'  => 'Test Module Site',
     'version'       => '0.1.0',
     'description'   => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    'author'        => 'Pihe Edmond <pihedy@gmail.com>',
+    'author'        => 'Pihe Edmond',
+    'email'         => 'pihedy@gmail.com',
     'side'          => 'site',
     'has_tab'       => true,
     'start'         => 'test.php'
